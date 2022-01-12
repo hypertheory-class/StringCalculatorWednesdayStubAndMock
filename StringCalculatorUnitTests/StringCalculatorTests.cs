121f352228268a236fc86e6c70b2da5d8e172f8f
Jeffry Gonzalez <jeff@hypertheory.com>
2022-01-12 15:39:18 -0500

    Initial commit

---
+++ StringCalculatorUnitTests/StringCalculatorTests.cs
@@ -0,0 +1,20 @@
+﻿
+using Xunit;
+
+// Instructions: https://osherove.com/tdd-kata-1
+
+namespace StringCalculatorUnitTests
+{
+    public class StringCalculatorTests
+    {
+        [Fact]
+        public void CalculatorReturnsZeroForEmptyString()
+        {
+            var calculator = new StringCalculator();
+
+            var result = calculator.Add("");
+
+            Assert.Equal(0, result);
+        }
+    }
+}
